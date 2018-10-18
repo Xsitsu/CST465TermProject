892eb6acdab9f62469cf083de2f00c2d182024f0
Xsitsu <jacoblocke21@live.com>
2018-10-17 22:03:00 -0700

    Commiting changes. Not working yet.

--- Assignment2/Startup.cs
+++ Assignment2/Startup.cs
@@ -20,6 +20,8 @@ namespace Assignment2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
