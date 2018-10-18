0c4e7d5287d2e7c8f2df49a472f6bfc5539c5fa8
Xsitsu <jacoblocke21@live.com>
2018-10-18 00:00:14 -0700

    Saving work.

--- Assignment2/Controllers/ContactController.cs
+++ Assignment2/Controllers/ContactController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Assignment2.Models;
 
 namespace Assignment2.Controllers
 {
     public class ContactController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Submit(ContactFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return Submit(model);
         }
     }
 }
---
+++ Assignment2/Models/ContactFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
